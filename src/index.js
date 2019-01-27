481cbfc84530792761c2fd31a5afe03b2a1552e1
Luiz Américo <luizamericop@gmail.com>
2019-01-27 08:37:27 -0300

    Update lit-element to latest version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import { init } from 'snabbdom'
 import vnode from 'snabbdom/vnode'
-import { UpdatingElement } from '@polymer/lit-element/lib/updating-element.js'
-export * from '@polymer/lit-element/lib/decorators.js'
+import { UpdatingElement } from 'lit-element/lib/updating-element.js'
+export * from 'lit-element/lib/decorators.js'
 
 export function initElementClass (modules) {
   const patch = init(modules)
   return class SnabbdomElement extends UpdatingElement {
+    /**
+    * Performs element initialization. By default this calls `createRenderRoot`
+    * to create the element `renderRoot` node and captures any pre-set values for
+    * registered properties.
+    */
+    initialize () {
+      super.initialize()
+      this.renderRoot = this.createRenderRoot()
+    }
+
+    /**
+       * Returns the node into which the element should render and by default
+       * creates and returns an open shadowRoot. Implement to customize where the
+       * element's DOM is rendered. For example, to render into the element's
+       * childNodes, return `this`.
+       * @returns {Element|DocumentFragment} Returns a node into which to render.
+       */
+    createRenderRoot () {
+      return this.attachShadow({ mode: 'open' })
+    }
+
     /**
     * Updates the element. This method reflects property values to attributes
     * and calls `render` to render DOM via snabbdom. Setting properties inside
